b418d3d73a2723d97d56ba4cc1a654f17bf98d05
Matthew R Laue <mattlaue@gmail.com>
2023-01-04 07:03:31 -0800

    Initial strauss.

--- examples/logging/autoload.php
+++ examples/logging/autoload.php
@@ -4,5 +4,4 @@ declare(strict_types=1);
 use examples\logging\vendor\mindspun\framework\Autoloader;
 
 require_once( __DIR__ . '/vendor_prefixed/mindspun/framework/Autoloader.php' );
-Autoloader::autoload( 'examples\logging\vendor', __DIR__ . '/vendor_prefixed');
 Autoloader::autoload( 'examples\logging', __DIR__ . '/includes' );
