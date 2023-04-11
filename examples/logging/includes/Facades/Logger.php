cca0a0ff400bf5c2598d6e91770d2f5a0ad13dcc
Matthew R. Laue <mattlaue@gmail.com>
2023-04-11 08:38:37 -0700

    Fix a typo in the Logger facade.

--- examples/logging/includes/Facades/Logger.php
+++ examples/logging/includes/Facades/Logger.php
@@ -5,7 +5,7 @@ namespace Examples\Logging\Facades;
 use Examples\Logging\Vendor\Mindspun\Framework\Facade;
 
 /**
- * PSR4 Logger facade.
+ * PSR3 Logger facade.
  *
  * @method static void emergency( string $message, array $context = array() )
  * @method static void alert( string $message, array $context = array() )
